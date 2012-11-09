8d889edc9d8494568f7bcd6d8b2ab287063fc522
polobaston73 <p02030507111317@yahoo.fr>
2012-11-09 18:54:52 +0100

    Mise à jour
    Ajout de la gestion par liste chainée.
    Ajout des trois fichiers CSV de test.

--- Info321-Tp4.adb
+++ Info321-Tp4.adb
@@ -11,9 +11,18 @@ procedure main is
 
 N: constant integer := 100;
 
+type ligne is array(1..6) of unbounded_string;
+type chaine;
+type ptr is access chaine;
+type chaine is record
+	data: ligne;
+	next: ptr;
+	end record;
+	
 type matrice is array(1..N, 1..6) of unbounded_string;
 
 mat : matrice;
+tab: ptr;
 
 function lire_CSV(URL: string) return matrice is
 	fichier : File_Type;
@@ -35,8 +44,8 @@ function lire_CSV(URL: string) return matrice is
 					prev_car := car + 1;
 					cptr_col := cptr_col + 1;
 				end if;
-				Result(cptr_ligne, cptr_col) := to_unbounded_string(slice(ligne_actuelle, prev_car, length(ligne_actuelle)));
 			end loop;
+			Result(cptr_ligne, cptr_col) := to_unbounded_string(slice(ligne_actuelle, prev_car, length(ligne_actuelle)));
 		cptr_ligne := cptr_ligne + 1;
 		exit when end_of_file(fichier);
 		end loop;
@@ -44,7 +53,124 @@ function lire_CSV(URL: string) return matrice is
 	return Result;
 	end;
 
+function  lire_CSV_LC(URL: String) return ptr is
+	fichier : File_Type;
+	ligne_actuelle : unbounded_string;
+	cptr_col : integer;
+	prev_car : integer;
+	anchor : ptr;
+	ptrs : ptr;
+	begin
+	Open(Name => URL, File => fichier, Mode => in_file);
+	ptrs := new chaine;
+	anchor := ptrs;
+	cptr_col := 1;
+	loop
+		ligne_actuelle := get_line(fichier);
+		cptr_col := 1;
+		prev_car := 1;
+		for car in 1..length(ligne_actuelle) loop
+			if element(ligne_actuelle, car) = ',' then
+				ptrs.data(cptr_col) := to_unbounded_string(slice(ligne_actuelle, prev_car, car - 1));
+				prev_car := car + 1;
+				cptr_col := cptr_col + 1;
+			end if;
+		end loop;
+		ptrs.data(cptr_col) := to_unbounded_string(slice(ligne_actuelle, prev_car, length(ligne_actuelle)));
+		ptrs.next:= new chaine;
+		ptrs := ptrs.next;
+		exit when end_of_file(fichier);
+		end loop;
+	close(fichier);
+	return anchor;
+	end;
+
+function afficher_routes(verbose: boolean) return ptr is
+	ptrs : ptr;
+	result: ptr;
+	begin
+		ptrs := lire_CSV_LC("routes.txt");
+		result := ptrs;
+		if verbose then 
+			while ptrs /= null loop
+				for i in 1..6 loop
+					put(ptrs.data(i)); put("   ");
+				end loop;
+				ptrs := ptrs.next;
+				new_line;
+			end loop;
+		end if;
+		return result;
+	end;
+
+function afficher_arrets (verbose: boolean) return ptr is
+	ptrs : ptr;
+	result: ptr;
+	begin
+		ptrs := lire_CSV_LC("stops.txt");
+		result := ptrs;
+		if verbose then
+			while ptrs /= null loop
+				for i in 1..6 loop
+					put(ptrs.data(i)); put("   ");
+				end loop;
+				ptrs := ptrs.next;
+				new_line;
+			end loop;
+		end if;
+		return result;
+	end;
+
+	function afficher_trajets (verbose: boolean) return ptr is
+	ptrs : ptr;
+	result: ptr;
+	begin
+		ptrs := lire_CSV_LC("stop_times.txt");
+		result := ptrs;
+		if verbose then
+			while ptrs /= null loop
+				for i in 1..6 loop
+					put(ptrs.data(i)); put("   ");
+				end loop;
+				ptrs := ptrs.next;
+				new_line;
+			end loop;
+		end if;
+		return result;
+	end;
+	
+function rechercher_arret(nom: unbounded_string) return unbounded_string is
+	arret: ptr;
+	begin
+		arret := afficher_routes(false);
+		while arret.data(2) /= nom and arret /= null loop
+			arret := arret.next;
+		end loop;
+		if arret = null then
+			return "None";
+		else
+			return arret.data(1);
+		end if;
+	end;
+
+function rechercher_trajets(verbose: boolean) return ptr is
+--TODO--
+
+procedure afficher_trajets(nom: unbounded_string) is
+	routes : ptr;
+	stop_id: unbounded_string;
+	begin
+		stop_id := rechercher_arret(nom);
+		if stop_id = "None" then
+			exit;
+		end if;
+		routes := afficher_routes(false);
+		while routes /= null loop
+			if route.data(1) = stop_id then
+		end loop;
+		--TODO--
+	end;
+	
 begin
-	mat := lire_CSV("toto.csv");
-	for i in 1..N loop for j in 1..6 loop put(mat(i,j));put(" "); end loop; end loop;
+	--TODO--
 end;
