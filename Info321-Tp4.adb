417d4a7685ed869a3f5c9c7ddd0288ea16e9878f
polobaston73 <p02030507111317@yahoo.fr>
2012-11-16 18:47:45 +0100

    Fin du Tp4
    Utilisation de listes chainées pour gestion de fichiers GTFS

--- Info321-Tp4.adb
+++ Info321-Tp4.adb
@@ -18,13 +18,18 @@ type chaine is record
 	data: ligne;
 	next: ptr;
 	end record;
-	
+
+type vehicules is (Tramway, Metro, Train, Bus, Ferry, Cablecars, Telecabine, Funiculaire);
+package enum_vehicule is new enumeration_io(vehicules);
+use enum_vehicule;
+
 type matrice is array(1..N, 1..6) of unbounded_string;
 
 mat : matrice;
 tab: ptr;
 
 function lire_CSV(URL: string) return matrice is
+-- Lis un fichier CSV et renvoie son contenu sous forme de matrice.
 	fichier : File_Type;
 	ligne_actuelle : unbounded_string;
 	cptr_ligne : integer;
@@ -54,6 +59,7 @@ function lire_CSV(URL: string) return matrice is
 	end;
 
 function  lire_CSV_LC(URL: String) return ptr is
+-- Lis un fichier CSV et renvoie son contenu sous forme de liste chaînée.
 	fichier : File_Type;
 	ligne_actuelle : unbounded_string;
 	cptr_col : integer;
@@ -62,13 +68,14 @@ function  lire_CSV_LC(URL: String) return ptr is
 	ptrs : ptr;
 	begin
 	Open(Name => URL, File => fichier, Mode => in_file);
-	ptrs := new chaine;
-	anchor := ptrs;
+	anchor := null;
 	cptr_col := 1;
-	loop
+	ligne_actuelle := get_line(fichier);
+	while not End_of_File(fichier) loop
 		ligne_actuelle := get_line(fichier);
 		cptr_col := 1;
 		prev_car := 1;
+		ptrs := new chaine;
 		for car in 1..length(ligne_actuelle) loop
 			if element(ligne_actuelle, car) = ',' then
 				ptrs.data(cptr_col) := to_unbounded_string(slice(ligne_actuelle, prev_car, car - 1));
@@ -77,100 +84,171 @@ function  lire_CSV_LC(URL: String) return ptr is
 			end if;
 		end loop;
 		ptrs.data(cptr_col) := to_unbounded_string(slice(ligne_actuelle, prev_car, length(ligne_actuelle)));
-		ptrs.next:= new chaine;
-		ptrs := ptrs.next;
-		exit when end_of_file(fichier);
+		ptrs.next := anchor;
+		anchor := ptrs;
 		end loop;
 	close(fichier);
 	return anchor;
 	end;
 
-function afficher_routes(verbose: boolean) return ptr is
-	ptrs : ptr;
-	result: ptr;
+function lire_routes return ptr is
+--Lis le fichier "routes.txt" et renvoie le contenu sous forme de liste chaînée.
 	begin
-		ptrs := lire_CSV_LC("routes.txt");
-		result := ptrs;
-		if verbose then 
-			while ptrs /= null loop
-				for i in 1..6 loop
-					put(ptrs.data(i)); put("   ");
-				end loop;
-				ptrs := ptrs.next;
-				new_line;
-			end loop;
-		end if;
-		return result;
+		return lire_CSV_LC("routes.txt");
 	end;
 
-function afficher_arrets (verbose: boolean) return ptr is
-	ptrs : ptr;
-	result: ptr;
+function lire_arrets return ptr is
+--Lis le fichier "stops.txt" et renvoie le contenu sous forme de liste chaînée.
 	begin
-		ptrs := lire_CSV_LC("stops.txt");
-		result := ptrs;
-		if verbose then
-			while ptrs /= null loop
-				for i in 1..6 loop
-					put(ptrs.data(i)); put("   ");
-				end loop;
-				ptrs := ptrs.next;
-				new_line;
-			end loop;
-		end if;
-		return result;
+		return lire_CSV_LC("stops.txt");
 	end;
 
-	function afficher_trajets (verbose: boolean) return ptr is
-	ptrs : ptr;
-	result: ptr;
+function lire_trajets return ptr is
+--Lis le fichier "stop_times" et renvoie le contenu sous forme de liste chaînée.
 	begin
-		ptrs := lire_CSV_LC("stop_times.txt");
-		result := ptrs;
-		if verbose then
-			while ptrs /= null loop
-				for i in 1..6 loop
-					put(ptrs.data(i)); put("   ");
-				end loop;
-				ptrs := ptrs.next;
-				new_line;
-			end loop;
-		end if;
-		return result;
+		return lire_CSV_LC("stop_times.txt");
 	end;
 	
-function rechercher_arret(nom: unbounded_string) return unbounded_string is
+function rechercher_ligne(nom: unbounded_string) return unbounded_string is
+-- Renvoie l'identifiant de la ligne correspondant au nom donné.
 	arret: ptr;
 	begin
-		arret := afficher_routes(false);
-		while arret.data(2) /= nom and arret /= null loop
+		arret := lire_routes;
+		while  arret /= null and then arret.data(2) /= nom loop
 			arret := arret.next;
 		end loop;
 		if arret = null then
-			return "None";
+			return to_unbounded_string("None");
 		else
 			return arret.data(1);
 		end if;
 	end;
-
-function rechercher_trajets(verbose: boolean) return ptr is
---TODO--
-
-procedure afficher_trajets(nom: unbounded_string) is
-	routes : ptr;
-	stop_id: unbounded_string;
+	
+function rechercher_arret(nom: unbounded_string) return ptr is
+-- Renvoie la liste des arrêts ayant pour nom le nom donné.
+	arret: ptr;
+	result: ptr;
+	pt_temp : ptr;
 	begin
-		stop_id := rechercher_arret(nom);
-		if stop_id = "None" then
-			exit;
-		end if;
-		routes := afficher_routes(false);
-		while routes /= null loop
-			if route.data(1) = stop_id then
+		result := null;
+		arret := lire_trajets;
+		while arret /= null loop
+			if arret.data(5) = nom then
+				pt_temp := new chaine;
+				pt_temp.data := arret.data;
+				pt_temp.next := result;
+				result := pt_temp;
+			end if;
+			arret := arret.next;
 		end loop;
-		--TODO--
+		return result;
+	end;
+
+function rechercher_trajets(route_id: unbounded_string) return ptr is
+-- Renvoie la liste de tous les arrêts d'une ligne donnée par l'identifiant.
+	arret: ptr;
+	result: ptr;
+	pt_temp : ptr;
+	begin
+		result := null;
+		arret := lire_trajets;
+		while arret /= null loop
+			if arret.data(2) = route_id then
+				pt_temp := new chaine;
+				pt_temp.data := arret.data;
+				pt_temp.next := result;
+				result := pt_temp;
+			end if;
+			arret := arret.next;
+		end loop;
+		return result;
+	end;
+
+function get_unbounded_string return unbounded_string is
+-- Procédure permettent de récupérer une chaine de caractère 
+	str: unbounded_string;
+	begin
+		skip_line;
+		str := get_line;
+		return str;
 	end;
 	
+procedure afficher_departs(nom: unbounded_string) is
+--  Affiche tous les départs d'un arrêt donné par son nom.
+	arret: ptr;
+	begin
+		new_line;
+		arret := rechercher_arret(nom);
+		put("Prochains départs à partir de l'arrêt "); put(nom); put(" :"); new_line;
+		while arret /= null loop
+			put("    - Heure de départ: "); put(arret.data(4)); new_line;
+			arret := arret.next;
+		end loop;
+		new_line;
+	end;	
+	
+procedure afficher_lignes is
+-- Affiche le contenu de "routes.txt"
+	route: ptr;
+	begin
+		new_line;
+		route := lire_routes;
+		while route /= null loop
+			put("Ligne: "); put(route.data(3)); put(" : "); new_line;
+			put("    - Identifiant: "); put(route.data(1)); new_line;
+			put("    - Nom court: "); put(route.data(2)); new_line;
+			put("    - Type de transport: "); put(vehicules'val(integer'value(to_string(route.data(4))))); new_line;
+			new_line;
+			route := route.next;
+		end loop;
+	end;
+
+procedure choix_possibles is
+-- 0: Quitter
+-- 1: Afficher toutes les lignes
+-- 2: Afficher une ligne en particulier
+-- 3: Donner tous les departs d'un arret
+	begin
+	put("Voici les actions possibles:"); new_line;
+	put("    1: Afficher toutes les lignes"); new_line;
+	put("    2: Afficher tous les arrets d'une ligne (entrer son nom)"); new_line;
+	put("    3: Afficher tous les départs d'un arret (donner son nom)"); new_line;
+	new_line;
+	put("    0: Quitter");new_line;
+	put("Quel est votre choix? ");
+	end;
+	
+procedure afficher_ligne(nom: unbounded_string) is
+-- Affiche tous les arrêts d'une ligne complète donnée par son identifiant.
+	arret: ptr;
+	begin
+		new_line;
+		arret := rechercher_trajets(rechercher_ligne(nom));
+		put("Ligne "); put(arret.data(2)); put(":"); new_line;
+		while arret /= null loop
+			put("  Nom de l'arret: "); put(arret.data(5)); new_line;
+			put("    - Heure d'arrivée: "); put(arret.data(3)); new_line;
+			put("    - Heure de départ: "); put(arret.data(4)); new_line;
+			new_line;
+			arret := arret.next;
+		end loop;
+	end;
+
+	pointeur : ptr;
+	choix: integer;
+	entree: unbounded_string;
+
 begin
-	--TODO--
+	choix_possibles;
+	get(choix);
+	while choix /= 0 loop
+		case choix is
+			when 1=> afficher_lignes;
+			when 2 => new_line; put("Donner la ligne désirée"); new_line; afficher_ligne(get_unbounded_string);
+			when 3 => new_line; put("Donner l'arret désirée"); new_line; afficher_departs(get_unbounded_string);
+			when others => put("Choix invalide");
+		end case;
+		choix_possibles;
+		get(choix);
+	end loop;
 end;
